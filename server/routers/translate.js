76d6d9e34b07fd2b77d177eff8f12d6bae18c419
Feben-A <afa508165@gmail.com>
2025-02-06 16:32:33 +0000

    Remove score router in translate.js

--- server/routers/translate.js
+++ server/routers/translate.js
@@ -3,6 +3,5 @@ const translateController = require("../controllers/translate");
 const translateRouter = Router();
 
 translateRouter.get("/:level", translateController.show);
-translateRouter.patch("/:score", );
 
 module.exports = translateRouter;
