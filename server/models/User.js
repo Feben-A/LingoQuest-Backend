66084ba6ea7453ac876b2460674e8a1f8acc9137
Feben-A <afa508165@gmail.com>
2025-02-06 13:54:21 +0000

    Add student login function to models

--- server/models/User.js
+++ server/models/User.js
@@ -9,6 +9,17 @@ class User {
       (this.password = password);
   }
 
+  static async getOneByStudentLogin(student_login) {
+    const response = await db.query(
+      "SELECT * FROM students WHERE student_login = $1;",
+      [student_login]
+    );
+    if (response.rows.length != 1) {
+      throw new Error("Student does not exist!");
+    }
+    return new User(response.rows[0]);
+  }
+
   static async marks(student_id) {
     const response = await db.query(
       "INSERT INTO marks (student_id, total_marks) VALUES ($1, $2) RETURNING *;",
